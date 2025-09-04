d724a4fc1f8f791be0ffcc72c3c0334271bb2100
lolo522 <lolo242834@hotmail.com>
2025-09-04 00:30:06 -0400

    Updated AdminPanel.tsx

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -21,6 +21,7 @@ export function AdminPanel() {
     addNotification,
     clearNotifications,
     exportSystemConfig,
+    exportCompleteSourceCode,
     importSystemConfig,
     syncWithRemote,
     syncAllSections
